b12a0f73de654c672f67f7d127d4587f01c44d10
VictorDMoura <mouramvictor@gmail.com>
2023-08-21 12:45:20 -0300

    connecting to the mongdb

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
 const path = require('path')
+const mongoose = require('mongoose')
+const Campgroud = require('./models/campground')
+
+const PORT = 3000
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+    console.log("Database connected")
+})
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -11,7 +23,15 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.listen(3000, () => {
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campgroud({title: 'My backyard', description: 'cheap camping'})
+    await camp.save()
+    res.send(camp)
+})
+
+
+
+app.listen(PORT, () => {
     console.log("Server ON!.")
     console.log('Port is', PORT)
 })
